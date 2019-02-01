eb97eb69860071e0f6d734bd6b09dc9aa2491d4b
william.lee <lyt0915@naver.com>
2019-02-01 23:43:22 +0900

    문제의 테스트 케이스는 모두 풀림, 그러나 오답. 여기서 마무리 지음.

--- 16236_baby_shark.cpp
+++ 16236_baby_shark.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
+int map[20][20];
+
 class CFish{
     public:
         int x;
@@ -13,15 +16,29 @@ class CFish{
             size = _size;
         }
         ~CFish(){
+            
+        }
+};
 
+class CShark: public CFish{
+    public:
+        int remain_exp;
+        CShark(int _x, int _y, int _size, int _exp): CFish(_x, _y, _size){
+            remain_exp = _exp;
+        }
+        void move(int _x, int _y){
+            map[y][x] = 0;
+            x = _x;
+            y = _y;
+            map[y][x] = 9;
         }
 };
 
 int main(){
     int N;
-    int map[20][20];
+    
     vector<CFish*> v_fishs;
-    CFish *baby_shark;
+    CShark *baby_shark;
 
     cin >> N;
 
@@ -33,62 +50,73 @@ int main(){
                 CFish *f = new CFish(j, i, size);
                 v_fishs.push_back(f);
             } else if ( size == 9 ) {
-                baby_shark = new CFish(j, i, 2);
+                baby_shark = new CShark(j, i, 2, 2);
             }
             map[i][j] = size;
         }
     }
 
-    
+    int mx[4] = {-1, 1, 0, 0};
+    int my[4] = {0, 0, -1, 1};
     int move = 0;
-    int remain_exp = 2;
-    while ( !v_fishs.empty() ){
+    while ( true ){
         CFish *close_fish = NULL;
         int closest_distance = 100;
         int closest_index = 0;
 
-        for ( int i=0; i < v_fishs.size(); i++){
-            int dx = v_fishs[i]->x - baby_shark->x;
-            int dy = v_fishs[i]->y - baby_shark->y;
-            if ( dx < 0 ) dx = -dx;
-            if ( dy < 0 ) dy = -dy;
+        queue<pair<int, int>> q;
+        q.push(make_pair(baby_shark->x, baby_shark->y));
+        
+        int moved[20][20] = {0, };
+        int move_num = 0;
+        bool is_find = false;
+        while ( !q.empty() ){
+            int now_x = q.front().first;
+            int now_y = q.front().second;
 
-            if ( closest_distance > dx + dy ){
-                CFish *temp = v_fishs[i];
-                if ( temp->size >= baby_shark->size) continue;
-                close_fish = temp;
-                closest_distance = dx + dy;
-                closest_index = i;
-            } else if ( closest_distance == dx + dy ){
-                if ( close_fish->y < v_fishs[i]->y ) continue;
-                if ( close_fish->x < v_fishs[i]->x ) continue;
-                if ( v_fishs[i]->size >= baby_shark->size) continue;
-                close_fish = v_fishs[i];
-                closest_index = i;
+            if ( is_find && move_num < moved[now_y][now_x]) break;
+
+            if ( map[now_y][now_x] != 0 && map[now_y][now_x] < baby_shark->size ) {
+                move_num = moved[now_y][now_x];
+                is_find = true;
             }
+
+            for ( int i=0; i < 4; i++){
+                int target_x = now_x + mx[i];
+                int target_y = now_y + my[i];
+                if ( target_x < 0 || target_x >= N || target_y < 0 || target_y >= N ) continue;
+                if ( moved[target_y][target_x] > 0 ) continue;
+                if ( map[target_y][target_x] > baby_shark->size) continue;
+
+                q.push(make_pair(target_x, target_y));
+                moved[target_y][target_x] = moved[now_y][now_x] + 1;
+            }
+
+            q.pop();
         }
 
-        if ( close_fish ){
-            remain_exp--;
-            if ( remain_exp == 0) {
-                baby_shark->size++;
-                remain_exp = baby_shark->size;
+        if ( move_num == 0 ) break;
+
+        is_find = false;
+        for ( int i=0; i < N; i++ ){
+            for ( int j=0; j < N; j++ ){
+                if ( moved[i][j] == move_num && map[i][j] != 0 && map[i][j] < baby_shark->size){
+                    baby_shark->move(j, i);
+                    move += moved[i][j];
+                    baby_shark->remain_exp--;
+                    if ( baby_shark->remain_exp == 0 ){
+                        baby_shark->size++;
+                        baby_shark->remain_exp = baby_shark->size;
+                    }
+                    is_find = true;
+                    break;
+                }
             }
-            cout << close_fish->size << "is dead." << close_fish->x << ", " << close_fish->y << "i'm "<< baby_shark->size << endl;
-           
-            move += closest_distance;
-            baby_shark->x = close_fish->x;
-            baby_shark->y = close_fish->y;
-            v_fishs.erase(v_fishs.begin() + closest_index);
-            delete close_fish;
-            
-        } else {
-            break;
+            if ( is_find ) break;
         }
-        close_fish == NULL;
     }
 
-    cout << move << endl;
+    cout << move;
 
     return 0;
 }
