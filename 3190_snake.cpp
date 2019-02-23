c023826fd99e59d8ef5ce340125a189a7e705aed
william.lee <lyt0915@naver.com>
2019-02-24 00:01:50 +0900

    3190 snake fix.

--- 3190_snake.cpp
+++ 3190_snake.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <deque>
+#include <vector>
 using namespace std;
 int N, K, L; // size, apple number, conversion number
 int map[100][100];
@@ -16,79 +17,58 @@ int main(){
   }
 
   deque<pair<int, int> > snake;
+  vector<pair<int, char> > command;
 
+  // init snake
   snake.push_back(make_pair(0, 0));
   map[0][0] = 9;
 
+  // init command
   cin >> L;
-
-  int dir = 0;
-  int count = 0;
-  int pre_count = 0;
-  int ans = 0;
-  bool find = false;
   for ( int l = 0; l < L; l++ ){
-    char input_dir;
-    cin >> count >> input_dir;
-
-    for ( int n = 0; n < count - pre_count; n++ ){
-      if ( !find ) ans++;
-      int headX = snake.back().first + dx[dir];
-      int headY = snake.back().second + dy[dir];
-
-      snake.push_back(make_pair(headX, headY));
-
-      if ( map[headY][headX] == 9 || headX < 0 || headX >= N || headY < 0 || headY >= N ){
-        find = true;
-        cout << ans << endl;
-        return 0;
-      }
-
-      if ( map[headY][headX] != 1 ){
-        int tailX = snake.front().first;
-        int tailY = snake.front().second;
-
-        map[tailY][tailX] = 0;
-        snake.pop_front();
-      }
-
-      map[headY][headX] = 9;
-    }
-
-    if ( input_dir == 'D' ){
-      dir++;
-    } else if ( input_dir == 'L' ){
-      dir--;
-    }
-
-    if ( dir < 0 ) {
-      dir += 4;
-    } else if ( dir >= 4 ) {
-      dir -= 4;
-    }
-    pre_count = count;
+    int count;
+    char dir;
+    cin >> count >> dir;
+    command.push_back(make_pair(count, dir));
   }
 
-  if ( !find ){
-    while ( true ){
-      ans++;
-      int headX = snake.back().first + dx[dir];
-      int headY = snake.back().second + dy[dir];
+  int dir = 0;
+  int cmd_idx = 0;
+  int ans = 0;
+  bool find = false;
+  while ( true ){
+    ans++;
+    int headX = snake.back().first + dx[dir];
+    int headY = snake.back().second + dy[dir];
 
-      if ( map[headY][headX] == 9 || headX < 0 || headX >= N || headY < 0 || headY >= N ) break;
-
-      snake.push_back(make_pair(headX, headY));
-
-      if ( map[headY][headX] != 1 ){
-        int tailX = snake.front().first;
-        int tailY = snake.front().second;
-
-        map[tailY][tailX] = 0;
-        snake.pop_front();
+    if ( command[cmd_idx].first == ans ){
+      char next_dir = command[cmd_idx].second;
+      if ( next_dir == 'D' ){
+        dir++;
+      } else if ( next_dir == 'L' ){
+        dir--;
       }
 
-      map[headY][headX] = 9;
+      if ( dir < 0 ) {
+        dir += 4;
+      } else if ( dir >= 4 ) {
+        dir -= 4;
+      }
+      cmd_idx++;
     }
+
+    if ( map[headY][headX] == 9 || headX < 0 || headX >= N || headY < 0 || headY >= N ) break;
+
+    if ( map[headY][headX] != 1 ){
+      int tailX = snake.front().first;
+      int tailY = snake.front().second;
+
+      map[tailY][tailX] = 0;
+      snake.pop_front();
+    }
+
+    snake.push_back(make_pair(headX, headY));
+    map[headY][headX] = 9;
   }
 
   cout << ans << endl;
