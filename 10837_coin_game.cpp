644675fdde1bdb3093ca645ea939ef2ed11e7826
william.lee <lyt0915@naver.com>
2019-05-27 11:30:29 +0900

    coin game complete

--- 10837_coin_game.cpp
+++ 10837_coin_game.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main(){
+  int round;
+  int game;
 
+  cin >> round >> game;
+
+  for ( int i = 0; i < game; i++ ){
+    int M, N;
+    bool success = true;
+    scanf("%d%d", &M, &N);
+    if ( M > N ){
+      if ( M - N > round - M + 2 ){
+        success = false;
+      }
+    } else if ( N > M ) {
+      if ( N - M > round - N + 1 ){
+        success = false;
+      }
+    }
+    if ( success ) {
+      printf("1\n");
+    } else {
+      printf("0\n");
+    }
+  }
   return 0;
 }
